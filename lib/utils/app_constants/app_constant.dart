fc5fcdb8252fc0eee67dd3599b491a4292778a53
NIranjan Thapa <niranjanthapa@NIranjans-MacBook-Air.local>
2023-02-28 19:17:14 +0545

    worked on app constants

--- lib/utils/app_constants/app_constant.dart
+++ lib/utils/app_constants/app_constant.dart
@@ -3,20 +3,6 @@ class AppConstant {
   static const String BASE_URL = "http://100.64.218.31:8080/cloudKitchen/";
   static const String API_VERSION = "/api/v1";
 
-  static const String USER_URI = "/user";
-  static const String SEARCH_FOODS_URI = "/menu";
-  static const String RESTAURANT_URI = "/restaurant";
-  static const String RESTAURANT_MENU_URI = "menu/restaurant/";
-  static const String PAYMENTS_URI = "/payment?username=";
-  static const String ORDERS_URI = "/customer/";
-  static const String CATEGORY_URI = "/category";
-  static const String MEAL_URI = "/meal";
-  static const String PAYPAL_PAYMENT_URI = "/payment/paypal";
-  static const String LOGIN_URI = "/user/login";
-  static const String REGISTRATION_URI = "/user/registration";
-  static const String STRIPE_PAYMENT_URI = "/payment/stripe";
-  static const String PROFILE_URI = "/user/";
-
   static const String USER_ID = "";
   static const String ORDER_ID = "";
   static const String MENU_FOOD_ID = "";
@@ -26,7 +12,30 @@ class AppConstant {
   static const String RESTAURANT_NAME = "";
   static const int PAGE = 0;
   static const int SIZE = 5;
+  static const String PERIOD = "";
+  static const String START_DATE = "";
+  static const String END_DATE = "";
+  static const String PAYMENT_METHOD = "";
+  static const String PAYMENT_PARTNER = "";
   static const String SORT_BY = "";
   static const String FOOD_NAME = "";
   static const String RESTAURANT_ID = "";
+  static const String USERNAME = "";
+
+  static const String USER_URI = "/user";
+  static const String SEARCH_FOODS_URI =
+      "/menu?foodName=$FOOD_NAME&sortBy=$SORT_BY&page=$PAGE&size=$SIZE&category=$CATEGORY&meal=$MEAL&rating=$RATING&restaurantName=$RESTAURANT_NAME";
+  static const String RESTAURANT_URI = "/restaurant";
+  static const String RESTAURANT_MENU_URI =
+      "menu/restaurant/$RESTAURANT_ID?foodName=$FOOD_NAME&sortBy=$SORT_BY&page=$PAGE&size=$SIZE&category=$CATEGORY&meal=$MEAL&rating=$RATING&";
+  static const String PAYMENTS_URI =
+      "/payment?page=$PAGE&startDate=$START_DATE&endDate=$END_DATE&username=$USERNAME&period=$PERIOD&paymentPartner=$PAYMENT_PARTNER&paymentMethod=$PAYMENT_METHOD&size=$SIZE";
+  static const String ORDERS_URI = "/customer/$USER_ID?page=$PAGE&size=$SIZE";
+  static const String CATEGORY_URI = "/category";
+  static const String MEAL_URI = "/meal";
+  static const String PAYPAL_PAYMENT_URI = "/payment/paypal";
+  static const String LOGIN_URI = "/user/login";
+  static const String REGISTRATION_URI = "/user/registration";
+  static const String STRIPE_PAYMENT_URI = "/payment/stripe";
+  static const String PROFILE_URI = "/user/$USER_ID";
 }

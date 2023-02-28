ebd2be5d571650d268d568099d313aed8644431a
NIranjan Thapa <niranjanthapa@NIranjans-MacBook-Air.local>
2023-02-28 18:37:37 +0545

    worked on app_constants

---
+++ lib/utils/app_constants/app_constant.dart
@@ -0,0 +1,19 @@
+class AppConstant {
+  static const APP_NAME = "";
+  static const BASE_URL = "";
+  static const API_VERSION = "";
+
+  static const USER_URI = "/";
+  static const SEARCH_FOODS_URI = "/menu";
+  static const RESTAURANT_URI = "/restaurant";
+  static const RESTAURANT_MENU_URI = "menu/restaurant/";
+  static const PAYMENTS_URI = "/payment?username=";
+  static const ORDERS_URI = "/customer/";
+  static const CATEGORY_URI = "/category";
+  static const MEAL_URI = "/meal";
+  static const PAYPAL_PAYMENT_URI = "/payment/paypal";
+  static const LOGIN_URI = "/user/login";
+  static const REGISTRATION_URI = "/user/registration";
+  static const STRIPE_PAYMENT_URI = "/payment/stripe";
+  static const PROFILE_URI = "/user/";
+}

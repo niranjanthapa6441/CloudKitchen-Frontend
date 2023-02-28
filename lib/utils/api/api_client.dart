c10ff83dcb40dfda74602a77ac872b3aafbe0a47
NIranjan Thapa <niranjanthapa@NIranjans-MacBook-Air.local>
2023-02-28 18:38:42 +0545

    worked on

---
+++ lib/utils/api/api_client.dart
@@ -0,0 +1,54 @@
+import 'package:get/get_connect.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+class APIClient extends GetConnect implements GetxService {
+  late String token;
+  final String appBaseUrl;
+  late Map<String, String> _mainHeaders;
+  Map<String, String> get mainHeaders => _mainHeaders;
+  APIClient({required this.appBaseUrl}) {
+    baseUrl = appBaseUrl;
+    timeout = Duration(seconds: 30);
+    token = "";
+    _mainHeaders = {
+      "content-type": "application/json; charset-UTF_8",
+      "Authorization": "Bearer $token"
+    };
+  }
+  void updateHeader(String token) {
+    _mainHeaders = {
+      "content-type": "application/json; charset-UTF_8",
+      "Authorization": "Bearer $token"
+    };
+  }
+
+  Future<Response> getData(String uri, Map<String, String> mainHeaders) async {
+    try {
+      Response response = await get(uri, headers: mainHeaders);
+      return response;
+    } catch (e) {
+      return Response(statusCode: 1, statusText: e.toString());
+    }
+  }
+
+  Future<Response> postData(String uri, dynamic body) async {
+    try {
+      Response response = await post(uri, body, headers: _mainHeaders);
+      return response;
+    } catch (e) {
+      return Response(statusCode: 1, statusText: e.toString());
+    }
+  }
+
+  Future<Response> deleteData(String uri, dynamic body) async {
+    try {
+      Response response = await delete(
+        uri,
+        headers: _mainHeaders,
+      );
+      return response;
+    } catch (e) {
+      return Response(statusCode: 1, statusText: e.toString());
+    }
+  }
+}
--- lib/utils/app_constants/app_constant.dart
+++ lib/utils/app_constants/app_constant.dart
@@ -1,19 +1,32 @@
 class AppConstant {
-  static const APP_NAME = "";
-  static const BASE_URL = "";
-  static const API_VERSION = "";
+  static const String APP_NAME = "Cloud Kitchen";
+  static const String BASE_URL = "http://100.64.218.31:8080/cloudKitchen/";
+  static const String API_VERSION = "/api/v1";
 
-  static const USER_URI = "/";
-  static const SEARCH_FOODS_URI = "/menu";
-  static const RESTAURANT_URI = "/restaurant";
-  static const RESTAURANT_MENU_URI = "menu/restaurant/";
-  static const PAYMENTS_URI = "/payment?username=";
-  static const ORDERS_URI = "/customer/";
-  static const CATEGORY_URI = "/category";
-  static const MEAL_URI = "/meal";
-  static const PAYPAL_PAYMENT_URI = "/payment/paypal";
-  static const LOGIN_URI = "/user/login";
-  static const REGISTRATION_URI = "/user/registration";
-  static const STRIPE_PAYMENT_URI = "/payment/stripe";
-  static const PROFILE_URI = "/user/";
+  static const String USER_URI = "/user";
+  static const String SEARCH_FOODS_URI = "/menu";
+  static const String RESTAURANT_URI = "/restaurant";
+  static const String RESTAURANT_MENU_URI = "menu/restaurant/";
+  static const String PAYMENTS_URI = "/payment?username=";
+  static const String ORDERS_URI = "/customer/";
+  static const String CATEGORY_URI = "/category";
+  static const String MEAL_URI = "/meal";
+  static const String PAYPAL_PAYMENT_URI = "/payment/paypal";
+  static const String LOGIN_URI = "/user/login";
+  static const String REGISTRATION_URI = "/user/registration";
+  static const String STRIPE_PAYMENT_URI = "/payment/stripe";
+  static const String PROFILE_URI = "/user/";
+
+  static const String USER_ID = "";
+  static const String ORDER_ID = "";
+  static const String MENU_FOOD_ID = "";
+  static const String CATEGORY = "";
+  static const String MEAL = "";
+  static const double RATING = 0.0;
+  static const String RESTAURANT_NAME = "";
+  static const int PAGE = 0;
+  static const int SIZE = 5;
+  static const String SORT_BY = "";
+  static const String FOOD_NAME = "";
+  static const String RESTAURANT_ID = "";
 }
